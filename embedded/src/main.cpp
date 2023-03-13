795641648dce00431d3976652c6df6cdf46d81d5
therealroxanne <roxannedaley054@gmail.com>
2023-03-13 00:11:09 -0500

    get and put requests updated

--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -2,11 +2,17 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
+#include <iostream>
+#include <cstdlib> // required for rand() and srand()
+#include <ctime> // required for time()
 #include "env.h"
-
+#define fanpin 22
+#define lightpin 23
 
 
 void setup() {
+  pinMode(fanpin, OUTPUT);
+  pinMode(lightpin, OUTPUT);
   // put your setup code here, to run once:
    Serial.begin(9600);
 	
@@ -26,4 +32,88 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(WiFi.status()== WL_CONNECTED){
+    HTTPClient http;
+    float temperature=random(21.0,33.0);
+    http.begin(endpoint);
+    http.addHeader("Content-type", "application/json");
+
+    StaticJsonDocument<128> doc;
+    String httpRequestData;
+
+    doc["Temperature"] = temperature;
+
+    serializeJson(doc, httpRequestData);
+    int httpResponseCode=http.PUT(httpRequestData);
+    String http_response;
+      if(httpResponseCode>0)
+    {
+      Serial.print("HTTP Response Code: ");
+      Serial.println(httpResponseCode);
+      Serial.print("Response from server");
+      http_response=http.getString();
+      Serial.println(http_response);
+
+    }
+    else{
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+
+      // Free resources
+      http.end();
+      // Stream& input;
+
+// Stream& input;
+http.begin(endpoint);
+  
+    int httpResponseCode = http.GET();
+
+    if (httpResponseCode>0) 
+    {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+
+        Serial.print("Response from server: ");
+        http_response = http.getString();
+        Serial.println(http_response);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+StaticJsonDocument<192> doc;
+
+DeserializationError error = deserializeJson(doc, http_response);
+
+if (error) {
+  Serial.print("deserializeJson() failed: ");
+  Serial.println(error.c_str());
+  return;
+}
+
+const char* id = doc["_id"]; // "640e1fc87a0bf6493917690b"
+const char* fan_state = doc["fan_state"]; // "True"
+const char* light_state = doc["light_state"]; // "False"
+
+if(fanpin==false)
+{
+  digitalWrite(fanpin, LOW);
+}
+else
+{
+  digitalWrite(fanpin, HIGH);
+}
+if(lightpin==false)
+{
+  digitalWrite(lightpin, LOW);
+}
+else
+{
+  digitalWrite(lightpin, HIGH);
+}
+  }
+  else{
+    return;
+  }
 }
